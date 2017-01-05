3b0d439bdd217965477d761681f8557a529434e4
Gleb Mikheev <mikheev@betaagency.ru>
2017-01-05 17:20:11 +0300

    первый коммит

---
+++ config/frontend.config.js
@@ -0,0 +1,16 @@
+
+
+const NODE_ENV = process.env.NODE_ENV ? process.env.NODE_ENV : 'development';
+
+module.exports = {
+  entry: {
+     app : './frontend/application.ts',
+    },
+  output: {
+    path: './www/',
+    publicPath: '/',
+    // при HMR нельзя у модуля использовать chunkhash, поэтому на тестовой среде это обычный хеш
+    filename: NODE_ENV === 'development' ? 'build/[name].[hash].js' : 'build/[name].[chunkhash].js',
+    chunkFilename: NODE_ENV === 'development' ? 'chunk/[id].[hash].js' : 'chunk/[id].[chunkhash].js',
+  },
+};
