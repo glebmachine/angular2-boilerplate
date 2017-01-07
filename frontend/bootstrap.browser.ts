37fc66d2daf3b3c1fe2a3e5758613911c32451c6
Gleb Mikheev <mikheev@betaagency.ru>
2017-01-07 14:28:42 +0300

    финально настроил компилятор

--- frontend/bootstrap.browser.ts
+++ frontend/bootstrap.browser.ts
@@ -1,5 +1,4 @@
 import 'core-js/es7/reflect'; // 2.3.1 -> 2.4.0
-import '@types/systemjs';
 
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule }              from './app.module';
